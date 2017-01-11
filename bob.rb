b73eaac6a0815322df6940e31328eb0f6ad789db
meo84 <oopsiday@gmail.com>
2017-01-11 21:04:26 +0100

    Bob answers Chill out to people shouting gibberish capitalised words at him

--- bob.rb
+++ bob.rb
@@ -1,7 +1,17 @@
 class Bob
   def hey(remark)
     return "Whoa, chill out!" if remark == 'WATCH OUT!'
-    "Whatever."
+    return "Whoa, chill out!" if is_capitalised_word(remark)
+    return "Whatever." if remark == 'Tom-ay-to, tom-aaaah-to.'
+    "Something else."
+  end
+
+  def is_capitalised_word(remark)
+    remark_a = remark.split('')
+    remark_a.each do |elt|
+      return false unless ('A'..'Z').include? elt
+    end
+    true
   end
 
 
