eb5df0a2f6593f9f63c8058a114b2f43fc97d624
meo84 <oopsiday@gmail.com>
2017-01-16 22:50:42 +0100

    Used regexp in is_capitalised_word method within Utilities string module

--- utilities/string.rb
+++ utilities/string.rb
@@ -1,10 +1,7 @@
 module Utilities
 	class String
 	   def self.is_capitalised_word?(line)
-	    line.split('').each do |elt|
-	      return false unless ('A'..'Z').include? elt
-	    end
-	    true
+	    ! (/^[A-Z]+$/.match(line).nil?)
 	  end
 	end
 end
